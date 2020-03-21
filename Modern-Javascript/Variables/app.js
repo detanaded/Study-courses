8c6b424aa7b41d5b911eea29e205e433e5d641cd
detanaded <48308236+detanaded@users.noreply.github.com>
2020-03-20 23:46:31 -0600

    Variables

---
+++ Modern-Javascript/Variables/app.js
@@ -0,0 +1,54 @@
+// var, let, const
+
+// var name = "Ernie Hernandez";
+// console.log(name);
+// name = "Steve Smith";
+// console.log(name);
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting ='Hello';
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Cannot start with a number
+
+
+// // Multi word vars
+
+// var firstName = 'John';  // Camel Case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tom'; // Pascal case
+
+// LET
+
+// let name = "Ernie Hernandez";
+// console.log(name);
+// name = "Steve Smith";
+// console.log(name);
+
+
+// CONST Constant = can't change or can't be reassigned
+
+
+// const name - 'John';
+// console.log(name);
+
+// // Cannot reassign
+// name = 'Sara';
+// // You'll notice in the console log error Assignment to constant variable or Missing initializer in const decleartion This is due to using Const and trying to reassign the variable.
+
+// const person = {
+//     name: 'John',
+//     age: 30
+// }
+// person.name =- 'Sara'
+// console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6)
+console.log(numbers);
+// numbers = [1,2,3,4,5,6]
+
+// You can mutate if its an object or an array, meaning adding info to the array and or object.
